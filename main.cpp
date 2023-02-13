c51d39ac540ec99bc214b1e9ebe01f7faddbda35
Ashuh <ashuh223@gmail.com>
2023-02-13 19:10:45 +0800

    Add ray.h

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include "color.h"
+#include "ray.h"
 #include <iostream>
+
+Color rayColor(const Ray &r) {
+    Vec3 unit_direction = unitVector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+}
 
 int main() {
     // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = Point3(0, 0, 0);
+    auto horizontal = Vec3(viewport_width, 0, 0);
+    auto vertical = Vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, focal_length);
 
     // Render
     std::cout << "P3\n"
@@ -13,8 +31,11 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            Color pixelColor(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
-            writeColor(std::cout, pixelColor);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            Color color = rayColor(r);
+            writeColor(std::cout, color);
         }
     }
 
---
+++ ray.h
@@ -0,0 +1,25 @@
+#ifndef RAYTRACER_RAY_H
+#define RAYTRACER_RAY_H
+
+#include "vec3.h"
+class Ray {
+public:
+    Ray() = default;
+
+    Ray(const Point3 &origin, const Vec3 &direction)
+        : orig(origin), dir(direction) {}
+
+    [[nodiscard]] Point3 origin() const { return orig; }
+
+    [[nodiscard]] Vec3 direction() const { return dir; }
+
+    [[nodiscard]] Point3 at(double t) const {
+        return orig + t * dir;
+    }
+
+private:
+    Point3 orig;
+    Vec3 dir;
+};
+
+#endif//RAYTRACER_RAY_H
