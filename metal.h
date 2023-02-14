fa20ee25b1d007c286fde81e9217c1530834f921
Ashuh <ashuh223@gmail.com>
2023-02-14 22:53:03 +0800

    Implement fuzzy reflections

--- metal.h
+++ metal.h
@@ -3,14 +3,15 @@
 
 #include "hit_record.h"
 #include "material.h"
+#include <cassert>
 
 class Metal : public Material {
 public:
-    explicit Metal(const Color &albedo) : Material(albedo) {}
+    explicit Metal(const Color &albedo, double f) : Material(albedo), fuzz(f) {}
 
     [[nodiscard]] std::optional<Ray> scatter(const Ray &r, const HitRecord &rec) const override {
         Vec3 reflected = reflect(unitVector(r.direction()), rec.normal);
-        auto scattered = Ray(rec.p, reflected);
+        auto scattered = Ray(rec.p, reflected + fuzz * randomInUnitSphere());
 
         if (dot(scattered.direction(), rec.normal) > 0) {
             return scattered;
@@ -18,6 +19,9 @@ public:
 
         return {};
     }
+
+private:
+    double fuzz;
 };
 
 #endif//RAYTRACER_METAL_H
